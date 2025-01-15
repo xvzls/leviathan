10a4404a36fa690728ba583c7fb4a87d44ea7e97
xvzls <xvzls@tuta.io>
2025-01-15 01:05:18 -0500

    Added benchmarking to build setup

--- setup.py
+++ setup.py
@@ -10,6 +10,22 @@ from typing import Literal, Any
 zig_mode: Literal['Debug', 'ReleaseSafe'] = "Debug"
 
 
+class LeviathanBench(Command):
+	user_options: list[Any] = []
+
+	def initialize_options(self) -> None:
+		pass
+
+	def finalize_options(self) -> None:
+		pass
+
+	def run(self) -> None:
+		self.run_command("build_ext")
+
+		build_lib_path = os.path.join("build", "lib")
+		errno = subprocess.call([sys.executable, "benchmark.py"], cwd=build_lib_path)
+		raise SystemExit(errno)
+
 class LeviathanTest(Command):
 	user_options: list[Any] = []
 
@@ -24,9 +40,9 @@ class LeviathanTest(Command):
 
 		self.run_command("build_ext")
 
-		errno = subprocess.call([sys.executable, "-m", "pytest", "-s"])
+		build_lib_path = os.path.join("build", "lib")
+		errno = subprocess.call([sys.executable, "-m", "pytest", "-s"], cwd=build_lib_path)
 		raise SystemExit(errno)
-
 
 class ZigBuildCommand(build_ext):
 	def run(self) -> None:
@@ -39,10 +55,18 @@ class ZigBuildCommand(build_ext):
 		src_path = os.path.join(build_dir, "libleviathan.so")
 		src_path2 = os.path.join(build_dir, "libleviathan_single_thread.so")
 
-		dest_path = os.path.join("leviathan", "leviathan_zig.so")
-		dest_path2 = os.path.join("leviathan", "leviathan_zig_single_thread.so")
+		dest_path = os.path.join("build", "lib", "leviathan", "leviathan_zig.so")
+		dest_path2 = os.path.join("build", "lib", "leviathan", "leviathan_zig_single_thread.so")
 		shutil.copyfile(src_path, dest_path)
 		shutil.copyfile(src_path2, dest_path2)
+
+		test_dest_path = os.path.join("build", "lib", "tests")
+		shutil.copytree("./tests", test_dest_path, dirs_exist_ok=True)
+
+		benchmarks_dest_path = os.path.join("build", "lib", "benchmarks")
+		shutil.copytree("./benchmarks", benchmarks_dest_path, dirs_exist_ok=True)
+		benchmark_py_dest_path = os.path.join("build", "lib", "benchmark.py")
+		shutil.copyfile("./benchmark.py", benchmark_py_dest_path)
 
 		st = os.stat(dest_path)
 		os.chmod(dest_path, st.st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
@@ -76,7 +100,7 @@ setup(
 	author_email="kike28.py@pm.me",
 	url="https://github.com/kython28/leviathan",
 	packages=find_packages(
-		exclude=["tests", "zig-out", "src"],
+		exclude=["tests", "benchmarks", "benchmark.py", "zig-out", "src"],
 		include=["leviathan", "leviathan.*"]
 	),
 	package_data={"leviathan": ["leviathan_zig.so"]},
@@ -84,6 +108,7 @@ setup(
 		"build_ext": ZigBuildCommand,
 		"develop": ZigDevelopCommand,
 		"install": ZigInstallCommand,
+		"bench": LeviathanBench,
 		"test": LeviathanTest
 	}
 )
